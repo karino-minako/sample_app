7a3966f3f6be8c79c120a0ac0d2b88f78f788bca
karino-minako <irikiiomo11@gmail.com>
2020-04-20 18:34:44 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '116f7fbbf55342b08302fb05788dd26e691841b4277a9af8a39b19a2ba4a4667b7d06ea5ce89a9af92d57e7f5b9fe198d716544ebd47619508caf888d1668865'
