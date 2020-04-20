6a9e977347a69ce9b9c4ced6ed0403458b60cb91
karino-minako <irikiiomo11@gmail.com>
2020-04-20 16:29:45 +0900

    [Finish] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
      get 'todolists/:id' => 'todolists#show', as: 'todolist'
 
+     get 'tidolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+
+     patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+
 end
