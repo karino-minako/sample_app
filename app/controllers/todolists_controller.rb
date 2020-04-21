8cd807bf5961272e22b8bff94872b809665d26c8
karino-minako <irikiiomo11@gmail.com>
2020-04-21 19:47:55 +0900

    [Finish] 3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -8,7 +8,7 @@ class TodolistsController < ApplicationController
         redirect_to todolist_path(list.id)
     end
     def index
-        @todolists = List.all
+        @lists = List.all
     end
     def show
         @list = List.find(params[:id])
@@ -20,6 +20,11 @@ class TodolistsController < ApplicationController
         list = List.find(params[:id])
         list.update(list_params)
         redirect_to todolist_path(list.id)
+    end
+    def destroy
+        list = List.find(params[:id]) #データ(レコード)を1件取得
+        list.destroy #データ（レコード）を削除
+        redirect_to todolists_path #List一覧画面へリダイレクト
     end
 
     private
